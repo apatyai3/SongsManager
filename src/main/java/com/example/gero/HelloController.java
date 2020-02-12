22b3788c44c63a8b2d5e5805016162978cc6e097
Watanabe Daiki <dwatanabe0209@outlook.jp>
2020-02-12 23:03:24 +0900

    top画面表示確認完了

--- src/main/java/com/example/gero/HelloController.java
+++ src/main/java/com/example/gero/HelloController.java
@@ -2,12 +2,16 @@ package com.example.gero;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
 
     @RequestMapping(value="/")
-    private String hello(){
-        return "Hello Worlds";
+    public ModelAndView  top(ModelAndView mav){
+    	mav.setViewName("top");
+        return mav;
     }
+    
+    
 }
