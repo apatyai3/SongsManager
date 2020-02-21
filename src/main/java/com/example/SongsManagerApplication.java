e22fe8e4de341ce8b960939467a1678408f11294
Watanabe Daiki <dwatanabe0209@outlook.jp>
2020-02-21 23:51:52 +0900

    ディレクトリ整理

--- src/main/java/com/example/SongsManagerApplication.java
+++ src/main/java/com/example/SongsManagerApplication.java
@@ -1,13 +1,13 @@
-package com.example.gero;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeroApplication {
+public class SongsManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GeroApplication.class, args);
+		SpringApplication.run(SongsManagerApplication.class, args);
 	}
 
 }
