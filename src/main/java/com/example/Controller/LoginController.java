b9bae6201bcd1a180e83f58f085ca07d60279879
Watanabe Daiki <dwatanabe0209@outlook.jp>
2020-02-22 00:56:17 +0900

    a

--- src/main/java/com/example/Controller/LoginController.java
+++ src/main/java/com/example/Controller/LoginController.java
@@ -1,14 +1,13 @@
 package com.example.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/")
-	public String login(Model model){
+	public String login(){
         return "login.html";
     }
 	
