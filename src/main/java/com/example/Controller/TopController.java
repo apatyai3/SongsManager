870b6fbd2b596781683be7e1d8addd202b5fc02f
Watanabe Daiki <dwatanabe0209@outlook.jp>
2020-03-03 22:05:29 +0900

    Fromクラス作成

--- src/main/java/com/example/Controller/TopController.java
+++ src/main/java/com/example/Controller/TopController.java
@@ -1,14 +1,18 @@
 package com.example.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.Form.loginForm;
 
 @Controller
 public class TopController {
 
 	@PostMapping("/top")
-	public String top(Model model){
+	public String top(@ModelAttribute("loginForm") loginForm loginForm){
+		String userName = loginForm.getUserName();
+		
         return "top.html";
     }
 }
